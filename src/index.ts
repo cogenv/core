6af171bcd8e81adf6186424a71b5dcd9cfad6c37
Yoni Calsin <yonicb20@gmail.com>
2020-03-22 12:24:19 -0500

    fix: 🐛 Changed flobal var name, and aggregated options under an object, for parse function, and aggregated validation for parsse objects !

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,18 @@ import { Merge } from 'merge-options-default';
 
 // Interfaces
 declare var global: {
-   cogenv: NodeJS.Process;
+   cog: NodeJS.Process;
 };
-interface CogenvOptions {
-   path?: '.env';
-   encoding?: 'utf8';
-   matchLine?: 'all' | 'normal';
+interface ParseOptions {
+   types?: boolean;
+   objects?: boolean;
    interpolatePrefix?: string;
 }
+interface CogenvOptions extends ParseOptions {
+   path?: '.env';
+   encoding?: 'utf8';
+}
+
 interface More {
    [key: string]: any;
 }
@@ -25,131 +29,152 @@ const NEWLINES_MATCH = /\n|\r|\r\n/;
 const defaultOptions: CogenvOptions = {
    path: '.env',
    encoding: 'utf8',
-   matchLine: 'normal',
    interpolatePrefix: '$',
+   types: false,
+   objects: false,
 };
 let database: More = {};
 let allPayload: More = {};
 
 // Designed the variables a value
-global.cogenv = process;
+global.cog = process;
 
 export const Parse = (
    source: string,
-   matchLine: 'all' | 'normal' = 'normal',
-   interpolatePrefix?: string,
-) => {
-   const payload: More = {};
-   const arr = source.toString().split(NEWLINES_MATCH);
-   const RegexInterpolate = new RegExp(
-      `(.?\\${interpolatePrefix}{?(?:[a-zA-Z0-9_]+)?}?)`,
-      'g',
-   );
-   const RegexInterpolateParts = new RegExp(
-      `(.?)\\${interpolatePrefix}{?([a-zA-Z0-9_]+)?}?`,
-      'g',
-   );
+   { interpolatePrefix, types, objects }: ParseOptions,
+) =>
+   // matchLine: 'all' | 'normal' = 'normal',
+   // interpolatePrefix?: string,
+   {
+      const payload: More = {};
+      const arr = source.toString().split(NEWLINES_MATCH);
 
-   const toValue = (val: string): string => {
-      const end = val.length - 1;
-      const isDoubleQuoted = val[0] === '"' && val[end] === '"';
-      const isSingleQuoted = val[0] === "'" && val[end] === "'";
+      const RegexInterpolate = new RegExp(
+         `(.?\\${interpolatePrefix}{?(?:[a-zA-Z0-9_]+)?}?)`,
+         'g',
+      );
+      const RegexInterpolateParts = new RegExp(
+         `(.?)\\${interpolatePrefix}{?([a-zA-Z0-9_]+)?}?`,
+         'g',
+      );
 
-      // if single or double quoted, remove quotes
-      if (isSingleQuoted || isDoubleQuoted) {
-         val = val.substring(1, end);
+      const toValue = (val: string): string => {
+         const end = val.length - 1;
+         const isDoubleQuoted = val[0] === '"' && val[end] === '"';
+         const isSingleQuoted = val[0] === "'" && val[end] === "'";
 
-         // if double quoted, expand newlines
-         if (isDoubleQuoted) {
-            val = val.replace(RE_NEWLINES, NEWLINE);
-         }
-      } else {
-         // remove surrounding whitespace
-         val = val.trim();
-      }
-      val = parseInterpolate(val);
-      return val;
-   };
+         // if single or double quoted, remove quotes
+         if (isSingleQuoted || isDoubleQuoted) {
+            val = val.substring(1, end);
 
-   const parseInterpolate = (source: string) => {
-      if (typeof source !== 'string') {
-         return source;
-      }
-
-      var matches: any[] = source.match(RegexInterpolate) || [];
-
-      if (matches.length > 0) {
-         return matches.reduce(function(newEnv, match) {
-            var parts = RegexInterpolateParts.exec(match);
-            var prefix = parts[1];
-
-            var value, replacePart;
-
-            if (prefix === '\\') {
-               replacePart = parts[0];
-               value = replacePart.replace(
-                  `\\${interpolatePrefix}`,
-                  interpolatePrefix,
-               );
-            } else {
-               var key = parts[2];
-               replacePart = parts[0].substring(prefix.length);
-
-               value = allPayload[key] || payload[key];
-
-               // process.env value 'wins' over .env file's value
-
-               // Resolve recursive interpolations
-               value = parseInterpolate(value);
+            // if double quoted, expand newlines
+            if (isDoubleQuoted) {
+               val = val.replace(RE_NEWLINES, NEWLINE);
             }
+         } else {
+            // remove surrounding whitespace
+            val = val.trim();
+         }
+         val = parseInterpolate(val);
+         return val;
+      };
 
-            return newEnv.replace(replacePart, value);
-         }, source);
-      } else {
-         return source;
-      }
-   };
+      const parseInterpolate = (source: string) => {
+         if (typeof source !== 'string') {
+            return source;
+         }
 
-   for (const v of arr) {
-      const keyValueArr = v.match(PARSE_MATCH_LINE);
+         var matches: any[] = source.match(RegexInterpolate) || [];
 
-      if (keyValueArr != null) {
-         let [z, key, value] = keyValueArr;
-         value = toValue(value);
-         payload[key] = value;
-         allPayload[key] = value;
-      } else {
+         if (matches.length > 0) {
+            return matches.reduce(function(newEnv, match) {
+               var parts = RegexInterpolateParts.exec(match);
+               var prefix = parts[1];
+
+               var value, replacePart;
+
+               if (prefix === '\\') {
+                  replacePart = parts[0];
+                  value = replacePart.replace(
+                     `\\${interpolatePrefix}`,
+                     interpolatePrefix,
+                  );
+               } else {
+                  var key = parts[2];
+                  replacePart = parts[0].substring(prefix.length);
+
+                  value = allPayload[key] || payload[key];
+
+                  // process.env value 'wins' over .env file's value
+
+                  // Resolve recursive interpolations
+                  value = parseInterpolate(value);
+               }
+
+               return newEnv.replace(replacePart, value);
+            }, source);
+         } else {
+            return source;
+         }
+      };
+
+      for (const v of arr) {
+         if (!v) {
+            continue;
+         }
+
+         const keyValueArr = v.match(PARSE_MATCH_LINE);
          let containType: any = /^\s*([\w.-]+)[:]\s*([a-z]+)\s*=\s*(.*)?\s*$/;
          containType = v.match(containType);
-         if (matchLine == 'all') {
-            payload['_types'] = { ...payload._types };
+
+         let matchObjectKey = v.match(/^\s*(^[\w\-\>]+)\s*=\s*(.*)?\s*$/);
+         let isObjectKey: any = false;
+         if (matchObjectKey) {
+            isObjectKey = matchObjectKey[1].split(/\-\>/gi);
+            isObjectKey = isObjectKey.length > 1;
          }
-         if (containType) {
+
+         if (keyValueArr != null) {
+            let [z, key, value] = keyValueArr;
+            value = toValue(value);
+            payload[key] = value;
+            allPayload[key] = value;
+         } else if (types && containType) {
+            payload['_types'] = { ...payload._types };
             let [z, key, type, value] = containType;
             value = toValue(value);
+
             allPayload[key] = value;
             payload[key] = value;
-            if (matchLine == 'all') {
+            if (types) {
                key = `${key}@${type}`;
                payload['_types'][key] = value;
             }
+         } else if (isObjectKey && objects) {
+            payload['_objects'] = { ...payload._objects };
+            let [z, key, value] = matchObjectKey;
+            value = toValue(value);
+            key = key.replace(/\-\>/, '@');
+            payload['_objects'][key] = value;
          }
       }
-   }
-   return payload;
-};
+      return payload;
+   };
 
 export const Config = (options: CogenvOptions = {}) => {
    options = Merge(defaultOptions, options);
+   const { path, encoding, types, objects } = options;
 
-   let cogenvPath = resolve(process.cwd(), options.path);
-   let encoding = options.encoding;
+   let cogenvPath = resolve(cog.cwd(), path);
 
    try {
-      let parsed: any = readFileSync(cogenvPath, { encoding });
-      parsed = Parse(parsed, options.matchLine, options.interpolatePrefix);
-      database = parsed;
-      cogenv.env = Merge(cogenv.env, database);
+      let parsed: string | More = readFileSync(cogenvPath, { encoding });
+      parsed = Parse(parsed, {
+         types,
+         objects,
+      });
+      // database = parsed;
+      // cogenv.env = Merge(cogenv.env, database);
       return { parsed };
    } catch (e) {
       return { error: e };
@@ -159,7 +184,7 @@ export const Config = (options: CogenvOptions = {}) => {
 export const Use = <T>(fn: Function, options?: T) => {
    const data = fn(database, options) || {};
    database = Merge(database, data);
-   cogenv.env = Merge(cogenv.env, database);
+   cog.env = Merge(cog.env, database);
 };
 
 export const Cogenv = {
